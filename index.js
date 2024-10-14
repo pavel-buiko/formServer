fd410b89a89d2947a26aa7acb0ef52d1723802ce
Pavel Buiko <pashabuiko17102004@gmail.com>
2024-10-14 19:06:09 +0300

    Fix: Add environment support

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import cors from "cors";
 import { PROJECTS_DATA } from "./items.js";
-
 import {
   addProjects,
   changeProjects,
   getProject,
 } from "./Controllers/serverController.js";
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/items", (req, res) => {
+  if (!PROJECTS_DATA) {
+    res.status(404).send("No projects found.");
+    return;
+  }
   res.json(PROJECTS_DATA);
 });
 
@@ -22,7 +28,7 @@ app.patch("/items/:id", changeProjects);
 
 app.get("/items/:id", getProject);
 
-const PORT = 5000;
+const PORT = process.env.PORT ?? 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
