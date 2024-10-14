0c10c3ffe128c2b933c1198215abb850751fb0ab
Pavel Buiko <pashabuiko17102004@gmail.com>
2024-10-14 19:36:02 +0300

    Add routes folder and functionality

--- index.js
+++ index.js
@@ -3,17 +3,13 @@ dotenv.config();
 import express from "express";
 import cors from "cors";
 import { PROJECTS_DATA } from "./items.js";
-import {
-  addProjects,
-  changeProjects,
-  getProject,
-} from "./Controllers/serverController.js";
+import router from "./routes/route.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/api", router);
 app.get("/items", (req, res) => {
   if (!PROJECTS_DATA) {
     res.status(404).send("No projects found.");
@@ -21,12 +17,6 @@ app.get("/items", (req, res) => {
   }
   res.json(PROJECTS_DATA);
 });
-
-app.post("/items", addProjects);
-
-app.patch("/items/:id", changeProjects);
-
-app.get("/items/:id", getProject);
 
 const PORT = process.env.PORT ?? 5000;
 app.listen(PORT, () => {
